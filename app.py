517239e053a0b6479f52a0edf21f5aa185e31242
mayurchati <167848006+mayurchati@users.noreply.github.com>
2024-04-24 00:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-# Example 1: Hello-Mayur
 print("Hello, Mayur!")
 # Example 2: Basic arithmetic operations
 a = 10
 b = 5
-sum = a + b
-difference = a - b
-product = a * b
-quotient = a / b
 print("Sum:", sum)
 print("Difference:", difference)
 print("Product:", product)
